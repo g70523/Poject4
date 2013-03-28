eaefbf1de546caec838cc5e049c71706261f57aa
Louise Whitney <louisewhitney@fullsail.edu>
2013-03-28 16:36:42 -0400

    Updated phone number function

--- Whitney_Louise.js
+++ Whitney_Louise.js
@@ -4,8 +4,32 @@ Scalable Data Infrastructures
 Project 4
 */
 
-var myLibrary = function(){
-	
-	
-	
+//Start Library
+
+// String Functions
+
+
+//Phone Number 
+var phoneNumber = function(phone) {
+
+	var a = phone.length;
+	var b = "-";
+	var c = b.indexOf("-");
+	var d = b.lastIndexOf("-");
+
+	if (a == 12 || c == 3 || d == 7) {
+		return true;
+	} else {
+		return false;
+	}
+
 };
+phoneNumber("123-456-7890");
+
+// Decimal  Places
+var decimalPoint = function(num){
+
+	var n=num.toFixed(3);
+	console.log(n);
+};
+decimalPoint(3.14159);
